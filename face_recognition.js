62ec2073d6452353bfde3c2e1bce439758aca9b5
James Taylor <j.taylor@kainos.com>
2020-01-26 15:13:40 +0000

    Add gender and age recognition

--- face_recognition.js
+++ face_recognition.js
@@ -5,7 +5,8 @@ Promise.all([
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
   faceapi.nets.faceExpressionNet.loadFromUri('/models'),
-  faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+  faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
+  faceapi.nets.ageGenderNet.loadFromUri('/models')
 ]).then(startVideo)
 
 async function getFaceMatcher() {
@@ -32,20 +33,24 @@ video.addEventListener('play', async () => {
   setInterval(async () => {
     const detections = await faceapi.detectAllFaces(video,
       new faceapi.TinyFaceDetectorOptions())
-      .withFaceLandmarks().withFaceExpressions().withFaceDescriptors()
+      .withFaceLandmarks().withFaceExpressions().withFaceDescriptors().withAgeAndGender()
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     //Clear the canvas so it can redraw itself
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     //Add the detections to the screen
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    // Could draw face landmarks here
+    // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
-    results.forEach((result, i) => {
+    //Manually add gender age and person prediction
+    resizedDetections.forEach((detection, i) => {
+      const match = faceMatcher.findBestMatch(detection.descriptor)
       const box = resizedDetections[i].detection.box
-      const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
+      const drawBox = new faceapi.draw.DrawBox(box, {
+        label: `${match} is a ${Math.round(detection.age)} year old ${detection.gender}`
+      })
       drawBox.draw(canvas)
     })
-  }, 100)
+  }, 1000)
 })
 
 function loadLabeledImages() {
